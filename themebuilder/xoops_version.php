0b5cf2a666dd6288b63822dd5537a79468cc3bb0
olivee <wajdi20tn@hotmail.com>
2014-04-27 15:32:55 +0200

    test1

--- themebuilder/xoops_version.php
+++ themebuilder/xoops_version.php
@@ -4,7 +4,7 @@ $modversion['name']        = _AM_SYSTEM_THEME;
 $modversion['version']     = '1.0';
 $modversion['description'] = _AM_SYSTEM_THEME_DESC;
 $modversion['author']      = '';
-$modversion['credits']     = 'The XOOPS Project; The MPN SE Project; Francisco Burzi <br>( http://phpnuke.org/ ); Kraven30 (Cointin Maxime); ';
+$modversion['credits']     = ' Olivee Wajdi';
 $modversion['help']        = 'page=themebuilder';
 $modversion['license'] = "GPL see LICENSE";
 $modversion['official']    = 1;
