d2806fa6eb8bbaa106be26576ba8aa0a14fd1aec
olivee <wajdi20tn@hotmail.com>
2014-04-27 16:05:37 +0200

    good  final commit

--- themebuilder/xoops_version.php
+++ themebuilder/xoops_version.php
@@ -4,7 +4,7 @@ $modversion['name']        = _AM_SYSTEM_THEME;
 $modversion['version']     = '1.0';
 $modversion['description'] = _AM_SYSTEM_THEME_DESC;
 $modversion['author']      = '';
-$modversion['credits']     = 'test  Olivee Wajdi test testt test';
+$modversion['credits']     = 'Olivee Wajdi';
 $modversion['help']        = 'page=themebuilder';
 $modversion['license'] = "GPL see LICENSE";
 $modversion['official']    = 1;
