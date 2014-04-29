1950446f485c8be51e00384e7f830d12bb1f71ed
olivee <wajdi20tn@hotmail.com>
2014-04-29 18:09:51 +0200

    1st update language/english/themebuilder.php

--- system/language/english/themebuilder.php
+++ system/language/english/themebuilder.php
@@ -4,129 +4,74 @@
  * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
  * _LANGCODE    en
  * _CHARSET     UTF-8
- * @version     $Id: themebuilder.php 11531 2013-05-12 07:46:06Z beckmi $
+ * @version     $Id: banners.php 9271 2012-04-04 04:31:29Z beckmi $
  */
+
+//%%%%%%        Admin Module Name  Banners         %%%%%
+define("_AM_SYSTEM_BANNERS_DBUPDATED",_AM_SYSTEM_DBUPDATED);
+
 //Nav
-define('_AM_SYSTEM_THEME_NAV_MANAGER',"Theme Builder");
-define('_AM_SYSTEM_THEME_NAV_LIST',"Theme Builder");
-define('_AM_SYSTEM_THEME_NAV_ADD',"Add a new Theme");
-define('_AM_SYSTEM_THEME_NAV_EDIT',"Edit Theme");
-define('_AM_SYSTEM_THEME_NAV_DELETE',"Delete Theme");
-// Tips
-define('_AM_SYSTEM_THEME_NAV_TIPS',"<ul><li>Add, update or delete Themes</li></ul>");
-//Infos
-define('_AM_SYSTEM_THEME',"System Theme");
-define('_AM_SYSTEM_THEME_DESC',"System Theme Description");
-
-define('_AM_SYSTEM_THEMEBUILDER_Smarty',"Smarty");
-define('_AM_SYSTEM_THEMEBUILDER_tableconfig_themeinstalee',"Theme Installed");
-define('_AM_SYSTEM_THEMEBUILDER_Conseil',"Dashboard?");
-define('_AM_SYSTEM_THEMEBUILDER_description',"Description");
-define('_AM_SYSTEM_THEMEBUILDER_tableconfig_theme_menudejainstallee',"Menu already installed");
-define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-define('_AM_SYSTEM_THEMEBUILDER_Submit',"Submit");
-
-define('_AM_SYSTEM_THEMEBUILDER_options_General',"General Options");
-define('_AM_SYSTEM_THEMEBUILDER_Favicon_Upload_File',"Favicon_Upload_File");
-define('_AM_SYSTEM_THEMEBUILDER_Favicon_Upload_File_img',"Favicon_Upload_File_img");
-define('_AM_SYSTEM_THEMEBUILDER_UseBoxedLayout',"UseBoxedLayout");
-define('_AM_SYSTEM_THEMEBUILDER_Favicon_Image_upload',"_Favicon_Image_upload");
-define('_AM_SYSTEM_THEMEBUILDER_Favicon_Image_url',"Favicon URL");
-define('_AM_SYSTEM_THEMEBUILDER_texture',"texture");
-define('_AM_SYSTEM_THEMEBUILDER_Apercu',"Overwiew");
-define('_AM_SYSTEM_THEMEBUILDER_repeat',"repeat");
-define('_AM_SYSTEM_THEMEBUILDER_position_repeat',"position_repeat");
-define('_AM_SYSTEM_THEMEBUILDER_scrollbgtexture',"scrollbgtexture");
-define('_AM_SYSTEM_THEMEBUILDER_enable_scroll_top',"enable_scroll_top");
-define('_AM_SYSTEM_THEMEBUILDER_size_font',"size_font");
-define('_AM_SYSTEM_THEMEBUILDER_effect_font',"effect_font");
-define('_AM_SYSTEM_THEMEBUILDER_use_font',"use_font");
-define('_AM_SYSTEM_THEMEBUILDER_colorbodybackg',"colorbodybackg");
-define('_AM_SYSTEM_THEMEBUILDER_olivee_itemq_BlockcolumnLeft_background_color',"olivee_itemq_BlockcolumnLeft_background_color");
-define('_AM_SYSTEM_THEMEBUILDER_blockTitle_color',"blockTitle_color");
-define('_AM_SYSTEM_THEMEBUILDER_blockTitle_background_color',"blockTitle_background_color");
-define('_AM_SYSTEM_THEMEBUILDER_font_blockTitle',"font_blockTitle");
-define('_AM_SYSTEM_THEMEBUILDER_size_blockTitle',"size_blockTitle");
-define('_AM_SYSTEM_THEMEBUILDER_textalign_blockTitle',"textalign_blockTitle");
-define('_AM_SYSTEM_THEMEBUILDER_olivee_itemq_BlockcolumnCenter_background_color',"livee_itemq_BlockcolumnCenter_background_color");
-define('_AM_SYSTEM_THEMEBUILDER_css_extra_text',"css_extra_text");
-define('_AM_SYSTEM_THEMEBUILDER_js_header_text',"js_header_text");
-define('_AM_SYSTEM_THEMEBUILDER_js_body_text',"js_body_text");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-//define('_AM_SYSTEM_THEMEBUILDER_Resume',"Summary");
-
-//icon format png
-
-//you can add more image to texture folder in the theme builder
-//
-//
-//choisissez le BG du body: you can add more image to bg folder in the theme builder
-//check this to activate bg rotate
-//body_background_img_repeat:
-//choisir la position repeat
-//choisir le scroll de bg texture
-//
-//code google analytique insert
-//facebook og activate
-//facebook_og_admins fb:admins
-//facebook_og_app_id fb:app_id
+define("_AM_SYSTEM_THEMEBUILDER_DASHBOARD","Dashboard");
+define("_AM_SYSTEM_THEMEBUILDER_Menu","Menu");
+define("_AM_SYSTEM_THEMEBUILDER_Slider","Slider");
+define("_AM_SYSTEM_THEMEBUILDER_Options","Options");
+define("_AM_SYSTEM_THEMEBUILDER_ThemeBuilder","Theme Builder");
+define("_AM_SYSTEM_THEMEBUILDER_Miseajour","Mise à jour");
+define("_AM_SYSTEM_THEMEBUILDER_apropos","A propos");
+define("_AM_SYSTEM_THEMEBUILDER_Index","Index");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN1","Vous pouvez creer des Menus.");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN2","Vous pouvez creer des Sliders.");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN3","Vous pouvez configurer le theme comme il vous semble.");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN4","Vous pouvez generer le theme");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN5","Vous pouvez ...");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN6","Vous pouvez ...");
+define("_AM_SYSTEM_THEMEBUILDER_YOUCAN7","Vous pouvez ...");
+define("_AM_SYSTEM_THEMEBUILDER_Smarty","Smarty");
+define("_AM_SYSTEM_THEMEBUILDER_description","Descriptions");
+define("_AM_SYSTEM_THEMEBUILDER_Resume","Resumé");
+define("_AM_SYSTEM_THEMEBUILDER_Conseil","you can use these smarty in any theme if you don't want to generate it automaticly. I will explane how...");
+define("_AM_SYSTEM_THEMEBUILDER_tableconfig_themeinstalee","table config_theme déjà installé");
+define("_AM_SYSTEM_THEMEBUILDER_inserttemplateok","INSERT_config_template_ok");
+define("_AM_SYSTEM_THEMEBUILDER_problemeinserttemp","probleme_INSERT_config_template.");
+define("_AM_SYSTEM_THEMEBUILDER_tableconfig_theme_menuinstallok","table config_theme_menu est installé");
+define("_AM_SYSTEM_THEMEBUILDER_installfail2","install failed2");
+define("_AM_SYSTEM_THEMEBUILDER_tableconfig_theme_menudejainstallee","table config_theme_menu déjà installé");
+define("_AM_SYSTEM_THEMEBUILDER_installfail1","install failed1");
+define("_AM_SYSTEM_THEMEBUILDER_css_EXTRA_modifie","css EXTRA modifié");
+define("_AM_SYSTEM_THEMEBUILDER_ProblememodificationCSSExtra","Probleme modification CSS Extra");
+define("_AM_SYSTEM_THEMEBUILDER_UseBoxedLayout","Use Boxed Layout: Choose Activate if you want to use the boxed layour instead of the full width.");
+define("_AM_SYSTEM_THEMEBUILDER_options_General","options Géneral");
+define("_AM_SYSTEM_THEMEBUILDER_Favicon_Image_upload","Favicon Image. Upload your desired favicon image or put url.");
+define("_AM_SYSTEM_THEMEBUILDER_Favicon_Image_url","Favicon Image URL:");
+define("_AM_SYSTEM_THEMEBUILDER_Favicon_Upload_File","Upload ICO File");
+define("_AM_SYSTEM_THEMEBUILDER_Favicon_Upload_File_img","Upload PNG File");
+define("_AM_SYSTEM_THEMEBUILDER_Apercu","Aperçu");
+define("_AM_SYSTEM_THEMEBUILDER_texture","choisissez la texture du body:");
+define("_AM_SYSTEM_THEMEBUILDER_repeat","choisir le repeat");
+define("_AM_SYSTEM_THEMEBUILDER_position_repeat","choisir la position repeat");
+define("_AM_SYSTEM_THEMEBUILDER_scrollbgtexture","choisir le scroll de bg texture");
+define("_AM_SYSTEM_THEMEBUILDER_colorbodybackg","color de body_background_color:");
+define("_AM_SYSTEM_THEMEBUILDER_enable_scroll_top","activer le Scroll top:");
+define("_AM_SYSTEM_THEMEBUILDER_size_font","size de font");
+define("_AM_SYSTEM_THEMEBUILDER_effect_font","effect de font");
+define("_AM_SYSTEM_THEMEBUILDER_use_font","choisir le font");
+define("_AM_SYSTEM_THEMEBUILDER_olivee_itemq_BlockcolumnLeft_background_color","color de olivee-itemq-BlockcolumnLeft_background_color:");
+define("_AM_SYSTEM_THEMEBUILDER_blockTitle_color","color de blockTitle_color:");
+define("_AM_SYSTEM_THEMEBUILDER_blockTitle_background_color","color de blockTitle_background_color:");
+define("_AM_SYSTEM_THEMEBUILDER_font_blockTitle","choisir le font de blockTitle");
+define("_AM_SYSTEM_THEMEBUILDER_size_blockTitle","size de font");
+define("_AM_SYSTEM_THEMEBUILDER_textalign_blockTitle","text align block title");
+define("_AM_SYSTEM_THEMEBUILDER_olivee_itemq_BlockcolumnCenter_background_color","color de olivee-itemq-BlockcolumnCenter_background_color:");
+define("_AM_SYSTEM_THEMEBUILDER_css_extra_text","css text extra a ajouter: Paste your custom CSS code here");
+define("_AM_SYSTEM_THEMEBUILDER_js_header_text","js header text extra a ajouter: Paste your custom js code here.");
+define("_AM_SYSTEM_THEMEBUILDER_js_body_text","js body text extra a ajouter: Paste your custom js code here");
+define("_AM_SYSTEM_THEMEBUILDER_Submit","Submit");
+define("_AM_SYSTEM_BANNERS_SUREDELBNR","Are you sure you want to delete this Client and ALL its Banners?");
+define("_AM_SYSTEM_BANNERS_EDITADVCLI","Edit Advertising Client");
+define("_AM_SYSTEM_BANNERS_EXTINFO","Extra Info:");
+define("_AM_SYSTEM_BANNERS_CHGCLI","Change Client");
+define("_AM_SYSTEM_BANNERS_USEHTML","Use HTML code?");
+define("_AM_SYSTEM_BANNERS_CODEHTML","Enter HTML code:");
 
 
-
-
-define('_AM_SYSTEM_THEMEBUILDER_INDEX',"Index");
-define('_AM_SYSTEM_THEMEBUILDER_DASHBOARD',"Dashboard");
-define('_AM_SYSTEM_THEMEBUILDER_MENU',"Menu");
-define('_AM_SYSTEM_THEMEBUILDER_OPTIONS',"Options");
-define('_AM_SYSTEM_THEMEBUILDER_THEMEBUILDER',"ThemeBuilder");
-define('_AM_SYSTEM_THEMEBUILDER_SLIDER',"Slider");
-define('_AM_SYSTEM_THEMEBUILDER_MISAJOUR',"Updated");
-define('_AM_SYSTEM_THEMEBUILDER_APROPOS',"About");
-
-
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN1',"YOUCAN1 ");
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN2',"YOUCAN2 ");
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN3',"YOUCAN3 ");
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN4',"YOUCAN4 ");
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN5',"YOUCAN5 ");
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN6',"YOUCAN6 ");
-define('_AM_SYSTEM_THEMEBUILDER_YOUCAN7',"YOUCAN7 ");
-
-//nouveau menu
-//cat_menu
-//cat skin
-//Direction Menu
-//Animation Menu
-//stickyoffset Menu: 0 inactif
-//Color Menu:
-
-//nouveau item dans un menu
-//menu parent
-//item parent
-//tittttre
-//link
-//ordre
-//icon
-//class parent item
-
-
-//Enregistrer
-
-//Ajouter un nouveau slide
-//titre slider
-//slider theme
-//
-//Ajouter un item au slider
-//Slider parent
-//Upload image
-//Upload File
-//image
-//titre de l'item dans le slider
-//url de l'item dans le slider
-//ordre de l'item slider
+?>
